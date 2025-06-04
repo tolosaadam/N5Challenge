8cb76e562a1cc3006214b1847c92eabe4c3d60da
Adam <67810957+tolosaadam@users.noreply.github.com>
2025-06-04 15:47:40 -0300

    feat () : endpoint de permissionType.

---
+++ N5Challenge.Api/src/N5Challenge.Api/.EndpointsDefinitions/PermissionType/Endpoints.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using N5Challenge.Api.Application.Permission.Queries.GetAll;
+using N5Challenge.Api.Application.PermissionType.Queries.GetAll;
+
+namespace N5Challenge.Api.EndpointsDefinitions.PermissionType;
+
+public class Endpoints
+{
+    internal static async Task<IResult> GetAll(
+    [FromServices] IMediator mediator,
+    [FromServices] IMapper autoMapper,
+    CancellationToken cancellationToken)
+    {
+        var query = new GetAllPermissionTypeQuery();
+        var result = await mediator.Send(query, cancellationToken);
+        var mappedResult = autoMapper.Map<IEnumerable<Responses.PermissionType.PermissionTypeResponse>>(result);
+        return Results.Ok(mappedResult);
+    }
+}
